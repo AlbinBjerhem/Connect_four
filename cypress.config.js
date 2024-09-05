6b848d087ef9253c8ab5d9392665327a74eead63
xfekke <fekke4201337@gmail.com>
2024-09-05 11:32:56 +0200

    Cypress install + cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,66 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
+const webpack = require("@cypress/webpack-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+const baseUrl = require('./baseUrl.js');
+
+module.exports = defineConfig(
+  {
+    e2e:
+    {
+      specPattern: '**/*.feature',
+      baseUrl,
+      video: false,
+      supportFile: false,
+      screenshotOnRunFailure: false,
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+
+        // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+        addCucumberPreprocessorPlugin(on, config);
+
+        on(
+          "file:preprocessor",
+          webpack(
+            {
+              webpackOptions:
+              {
+                resolve: {
+                  extensions: [".js"],
+                },
+                module: {
+                  rules:
+                    [
+                      {
+                        test: /\.feature$/,
+                        use:
+                          [
+                            {
+                              loader: "@badeball/cypress-cucumber-preprocessor/webpack",
+                              options: config,
+                            },
+                          ],
+                      },
+                    ],
+                },
+              },
+            }
+          )
+        );
+
+        on(
+          'task',
+          {
+            log(message) {
+              console.log(message)
+              return null
+            },
+          }
+        );
+
+        // Make sure to return the config object as it might have been modified by the plugin.
+        return config;
+      }
+    }
+  }
+);
