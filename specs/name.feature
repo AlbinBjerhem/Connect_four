0fa51c61b56dde4960edf8f2348fff8257feb561
xfekke <fekke4201337@gmail.com>
2024-09-05 12:48:28 +0200

    Update

--- specs/name.feature
+++ specs/name.feature
@@ -1,4 +1,4 @@
-Feature: Register player name and place a piece
+Feature: Register player name and declare winner
 
   Scenario: Player registers name before the game starts and places a piece
     Given that I am on the Connect Four game page
@@ -6,3 +6,10 @@ Feature: Register player name and place a piece
     Then I should see "Albin's turn" displayed on the screen
     When I place a piece in the first column
     Then I should see "Chalil's turn" displayed on the screen
+
+  Scenario: The game displays the winning player's name when the game is over
+    Given that I am on the Connect Four game page
+    When I click the "Play Game" button
+    And I simulate a fast game where Player 1 wins on the 4th round
+    Then I should see "Albin wins!" displayed on the screen
+    And the winner’s name should be saved in a variable
--- specs/step_definitions/name.js
+++ specs/step_definitions/name.js
@@ -1,12 +1,14 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
+// Scenario: Player registers name before the game starts and places a piece
+
 Given('that I am on the Connect Four game page', () => {
   cy.visit('http://localhost:5500'); 
 });
 
 When('I click the "Play Game" button', () => {
   cy.window().then((win) => {
-    // Name inputs
+
     cy.stub(win, 'prompt')
       .onFirstCall().returns('Albin')  // Player 1
       .onSecondCall().returns('Chalil'); // Player 2
@@ -25,4 +27,34 @@ When('I place a piece in the first column', () => {
 
 Then('I should see "Chalil\'s turn" displayed on the screen', () => {
   cy.get('#status').should('have.text', "Chalil's turn");
+});
+
+let displayWinner = '';
+
+// Scenario: The game displays the winning player's name when the game is over
+
+When('I simulate a fast game where Player 1 wins on the 4th round', () => {
+  // Player moves
+  cy.get('.cell[data-col="0"]').first().click();  
+  cy.get('.cell[data-col="1"]').first().click();  
+  cy.get('.cell[data-col="0"]').first().click();  
+  cy.get('.cell[data-col="1"]').first().click();  
+  cy.get('.cell[data-col="0"]').first().click();  
+  cy.get('.cell[data-col="1"]').first().click();  
+  cy.get('.cell[data-col="0"]').first().click();  
+
+  cy.get('#status').then(($status) => {
+    const statusText = $status.text();
+    if (statusText.includes('wins')) {
+      displayWinner = 'Albin'; 
+    }
+  });
+});
+
+Then('I should see "Albin wins!" displayed on the screen', () => {
+  cy.get('#status').should('have.text', "Albin wins!");
+});
+
+Then('the winner’s name should be saved in a variable', () => {
+  expect(displayWinner).to.equal('Albin'); 
 });
