541eb03a6e28c72629c3982032b7a4e17049996f
dmikozzz <146703887+dmikozzz@users.noreply.github.com>
2024-09-09 00:48:22 +0200

    update
    reusable steps
    vertical win

--- specs/step_definitions/wins.js
+++ specs/step_definitions/wins.js
@@ -8,6 +8,7 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return true;
 });
 
+//Reusable steps
 Given('that I am on the Connect Four game page', () => {
   cy.visit('http://localhost:5500');
 });
@@ -16,26 +17,39 @@ Given('I click the {string} button', () => {
   cy.get('#play-game').click();
 });
 
-Given('I enter {string} as Player 1 and {string} as Player 2', (Tintin, Snowy) => {
-  cy.get('input#player1').clear().type(Tintin); 
-  cy.get('input#player2').clear().type(Snowy); 
+Given('I enter {string} as Player 1 and {string} as Player 2', (player1, player2) => {
+  cy.get('input#player1').clear().type(player1);
+  cy.get('input#player2').clear().type(player2);
 });
 
 Given('I start the game', () => {
   cy.get('#startGameButton').click(); 
 });
 
+ // Player 1 wins horizontally
 When('I simulate a game where player 1 wins', () => {
-  cy.get('.cell[data-col="0"]').eq(5).click({ force: true }); 
-  cy.get('.cell[data-col="0"]').eq(5).click({ force: true }); 
-  cy.get('.cell[data-col="1"]').eq(5).click({ force: true }); 
-  cy.get('.cell[data-col="1"]').eq(5).click({ force: true }); 
-  cy.get('.cell[data-col="2"]').eq(5).click({ force: true }); 
-  cy.get('.cell[data-col="2"]').eq(5).click({ force: true }); 
-  cy.get('.cell[data-col="3"]').eq(5).click({ force: true }); 
-
+  cy.get('.cell[data-col="0"]').eq(5).click({ force: true });
+  cy.get('.cell[data-col="0"]').eq(5).click({ force: true });
+  cy.get('.cell[data-col="1"]').eq(5).click({ force: true });
+  cy.get('.cell[data-col="1"]').eq(5).click({ force: true });
+  cy.get('.cell[data-col="2"]').eq(5).click({ force: true });
+  cy.get('.cell[data-col="2"]').eq(5).click({ force: true });
+  cy.get('.cell[data-col="3"]').eq(5).click({ force: true });
 });
 
-Then('We should see a message saying {string}', () => {
-  cy.get('#status').should('have.text', "Tintin wins!");
+// Player 2 wins vertically
+When('I simulate a game where player 2 wins vertically', () => {
+  cy.get('.cell[data-col="0"]').eq(5).click({ force: true }); 
+  cy.get('.cell[data-col="1"]').eq(5).click({ force: true }); 
+  cy.get('.cell[data-col="0"]').eq(4).click({ force: true }); 
+  cy.get('.cell[data-col="1"]').eq(4).click({ force: true }); 
+  cy.get('.cell[data-col="0"]').eq(3).click({ force: true }); 
+  cy.get('.cell[data-col="1"]').eq(3).click({ force: true }); 
+  cy.get('.cell[data-col="2"]').eq(5).click({ force: true }); 
+  cy.get('.cell[data-col="1"]').eq(2).click({ force: true }); 
+});
+
+//Resuable to check status message
+Then('We should see a message saying {string}', (message) => {
+  cy.get('#status').should('have.text', message);
 });
--- specs/wins.feature
+++ specs/wins.feature
@@ -1,4 +1,4 @@
-Feature: Player wins by connecting four game pieces horizontally
+Feature: Player wins by connecting four game pieces 
 
   Scenario: Player 1 wins horizontally
     Given that I am on the Connect Four game page
@@ -7,3 +7,12 @@ Feature: Player wins by connecting four game pieces horizontally
     And I start the game
     When I simulate a game where player 1 wins
     Then We should see a message saying "Tintin wins!"
+
+
+  Scenario: Player 2 wins vertically
+    Given that I am on the Connect Four game page
+    And I click the "Play Game" button
+    And I enter "Tintin" as Player 1 and "Snowy" as Player 2
+    And I start the game
+    When I simulate a game where player 2 wins vertically
+    Then We should see a message saying "Snowy wins!"
