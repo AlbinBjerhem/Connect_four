b530b0cea23387acab670e5e41510fba92a8ef14
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2024-09-24 02:12:16 +0200

    commit

--- classes/external.js
+++ classes/external.js
@@ -8,7 +8,9 @@ export class External {
 
   async getMoveFromExternalAI(aiLevel = 1, state) {
     await this.helper.sleep(1000);
-    let response = await (await fetch('https://ludolab.net/solve/connect4?level=' + aiLevel + '&position=' + state)).json();
+    let response = await (await fetch('/solve/connect4?level=' + aiLevel + '&position=' + state)).json();
+    console.log("ai level: " + aiLevel + " state: " + state)
+
     let movesSortedByHowGood = response.sort((a, b) => a.score > b.score ? -1 : 1);
 
     return movesSortedByHowGood[0].move - 1;
