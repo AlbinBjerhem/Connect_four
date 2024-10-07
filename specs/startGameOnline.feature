abeb94f6588d65c89b62e526ace6696643f78173
AlbinBjerhem <albinbjerhem@gmail.com>
2024-10-07 11:47:44 +0200

    join online and create lobby works

--- specs/startGameOnline.feature
+++ specs/startGameOnline.feature
@@ -1,8 +1,15 @@
 Feature: Start Online Game
-  As two players, I want to be able to start an online game in a 4-in-a-row game
+  As two players, we want to create and join an online lobby to play a 4-in-a-row game.
 
-  Scenario: Both players start an online game
-    Given that both players have opened the game in their browsers
+  Scenario: Player 1 creates a lobby and Player 2 joins it
+    Given both players have opened the game in their browsers
     When Player 1 clicks on the "Play Online" button
-    And Player 2 clicks on the "Play Online" button
-    Then both players should be waiting for a game to start
+    And Player 1 clicks on the "Create Lobby" button
+    And Player 1 clicks on the "Continue" button
+    Then Player 1 should see the lobby code and save it
+    When Player 2 clicks on the "Play Online" button
+    And Player 2 clicks on the "Join Lobby" button
+    And Player 2 clicks on the "Continue" button
+    And Player 2 enters the saved lobby code and clicks "Continue"
+
+
--- specs/step_definitions/startGameOnline.js
+++ specs/step_definitions/startGameOnline.js
@@ -1,35 +1,78 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('that both players have opened the game in their browsers', () => {
-  // Visit the page with the two iframes for Player 1 and Player 2
+// Variable to store the lobby code
+let lobbyCode = '';
+
+Given('both players have opened the game in their browsers', () => {
+  // Visit the game page with the two iframes
   cy.visit('http://localhost:5173');
 });
 
 When('Player 1 clicks on the "Play Online" button', () => {
-  // Interact with Player 1's iframe and click the "Play Online" button
+  // Player 1 clicks "Play Online"
   cy.get('iframe#player1').then($iframe => {
     const player1Body = $iframe.contents().find('body');
     cy.wrap(player1Body).find('#play-online').click();
   });
 });
 
+When('Player 1 clicks on the "Create Lobby" button', () => {
+  // Player 1 clicks "Create Lobby"
+  cy.get('iframe#player1').then($iframe => {
+    const player1Body = $iframe.contents().find('body');
+    cy.wrap(player1Body).find('#createLobbyButton').click();
+  });
+});
+
+When('Player 1 clicks on the "Continue" button', () => {
+  // Player 1 clicks "Continue" after creating the lobby
+  cy.get('iframe#player1').then($iframe => {
+    const player1Body = $iframe.contents().find('body');
+    cy.wrap(player1Body).find('#createNameButton').click();
+  });
+});
+
+Then('Player 1 should see the lobby code and save it', () => {
+  // Player 1 sees the lobby code and saves it to a variable
+  cy.get('iframe#player1').then($iframe => {
+    const player1Body = $iframe.contents().find('body');
+    cy.wrap(player1Body).find('#lobbyCodeDisplay').invoke('text').then((code) => {
+      lobbyCode = code.trim(); // Save the lobby code
+    });
+  });
+});
+
 When('Player 2 clicks on the "Play Online" button', () => {
-  // Interact with Player 2's iframe and click the "Play Online" button
+  // Player 2 clicks "Play Online"
   cy.get('iframe#player2').then($iframe => {
     const player2Body = $iframe.contents().find('body');
     cy.wrap(player2Body).find('#play-online').click();
   });
 });
 
-Then('both players should be waiting for a game to start', () => {
-  // Verify that both players see a waiting state (this will depend on your specific UI)
-  cy.get('iframe#player1').then($iframe => {
-    const player1Body = $iframe.contents().find('body');
-    cy.wrap(player1Body).find('p:contains("Waiting for game to start...")');
-  });
-
+When('Player 2 clicks on the "Join Lobby" button', () => {
+  // Player 2 clicks "Join Lobby"
   cy.get('iframe#player2').then($iframe => {
     const player2Body = $iframe.contents().find('body');
-    cy.wrap(player2Body).find('p:contains("Waiting for game to start...")');
+    cy.wrap(player2Body).find('#joinLobbyButton').click();
   });
 });
+
+When('Player 2 clicks on the "Continue" button', () => {
+  // Player 1 clicks "Continue" after creating the lobby
+  cy.get('iframe#player2').then($iframe => {
+    const player1Body = $iframe.contents().find('body');
+    cy.wrap(player1Body).find('#joinNameButton').click();
+  });
+});
+
+When('Player 2 enters the saved lobby code and clicks "Continue"', () => {
+  // Player 2 enters the lobby code and clicks "Continue"
+  cy.get('iframe#player2').then($iframe => {
+    const player2Body = $iframe.contents().find('body');
+    cy.wrap(player2Body).find('#joinCodeInput').type(lobbyCode); // Type the saved lobby code
+    cy.wrap(player2Body).find('#joinCodeButton').click();
+  });
+});
+
+
